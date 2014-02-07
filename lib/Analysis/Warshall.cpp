f6dd909837f4e46f1811e3320a30ab708662e7ac
Gavin Gresham <silenthunter@gmail.com>
2014-02-06 20:44:43 -0500

    Warshall doesn't repeat cycles now

--- lib/Analysis/Warshall.cpp
+++ lib/Analysis/Warshall.cpp
@@ -8,8 +8,16 @@
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/Analysis/Dominators.h"
 #include "llvm/Analysis/CFG.h"
+#include <set>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
 
 using namespace llvm;
+using std::set;
+using std::vector;
+using std::string;
 
 STATISTIC(warshallLoop, "Number of loops found using the Warshall Algorithm");
 
@@ -20,14 +28,68 @@ namespace
 		static char ID;
 		Warshall() : FunctionPass(ID){}
 
+		set<BasicBlock*> visited;
+		set<string> cycles;
+
+		bool mycompr(string a, string b)
+		{
+			if(a != NULL)
+			return true;
+			return false;
+		}
+
+		bool isReachable(BasicBlock *from, BasicBlock *to)
+		{
+			visited.clear();
+			vector<string> path;
+			bool retn = isReachableRecurse(from, to, &path);
+
+			std::sort(path.begin(), path.end());
+
+			//Create a hash of the cycle based on an alphabatized list of block names
+			string hash = "";
+			for(vector<string>::iterator itr = path.begin(); itr != path.end(); itr++)
+				hash += *itr;
+
+			if(cycles.find(hash) != cycles.end())
+				return false;
+			else if(retn)
+				cycles.insert(hash);
+
+			return retn;
+		}
+
+		bool isReachableRecurse(BasicBlock *from, BasicBlock *to, vector<string> *path)
+		{
+			if(visited.find(from) != visited.end()) return false;
+			if(from == NULL) return false;
+			if(from == to)
+			{
+				path->push_back(from->getName());
+				return true;
+			}
+
+			visited.insert(from);
+
+			TerminatorInst *term = from->getTerminator();
+			int numSucc = term->getNumSuccessors();
+			for(int i = 0; i < numSucc; i++)
+			{
+				BasicBlock *child = term->getSuccessor(i);
+				bool reachable = isReachableRecurse(child, to, path);
+
+				if(reachable)
+				{
+					path->push_back(from->getName());
+					return true;
+				}
+			}
+
+			return false;
+		}
+
 		virtual bool runOnFunction(Function &F)
 		{
-			//Get dominator info
-			DominatorTree &tree = getAnalysis<DominatorTree>();
-
-			//Get loop info
-			LoopInfo &LI = getAnalysis<LoopInfo>();
-
 			Function::BasicBlockListType &blocks = F.getBasicBlockList();
 			for(Function::iterator itr = blocks.begin(); itr != blocks.end(); itr++)
 			{
@@ -38,7 +100,7 @@ namespace
 					for(int i = 0; i < succ; i++)
 					{
 						BasicBlock *succBlock = term->getSuccessor(i);
-						if(succBlock == &(*itr) && isPotentiallyReachable(&(*itr), &(*itr2), &tree, &LI))
+						if(succBlock == &(*itr) && isReachable(&(*itr), &(*itr2)))
 						{
 							errs() << /*F.getName() << "." << */itr->getName()<<" -> ";
 							errs() << /*F.getName() << "." << */itr2->getName()<<"\n";
@@ -48,6 +110,8 @@ namespace
 					
 				}
 			}
+
+			return false;
 		}
 
 		void getAnalysisUsage(AnalysisUsage &AU) const
